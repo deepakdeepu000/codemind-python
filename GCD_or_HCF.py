70e8ba90e3584fdea8ef708be49e391cbc106ef7
deepakdeepu000 <99535087+deepakdeepu000@users.noreply.github.com>
2022-05-29 22:48:47 +0530

    def gcd(a,b):     if a<b:         a,b=b,a     c=a     while True:         if a%c==0 and b%c==0:             return c         c-=1 a,b=map(int,input().split()) print(gcd(a,b))

---
+++ GCD_or_HCF.py
@@ -0,0 +1,10 @@
+def gcd(a,b):
+    if a<b:
+        a,b=b,a
+    c=a
+    while True:
+        if a%c==0 and b%c==0:
+            return c
+        c-=1
+a,b=map(int,input().split())
+print(gcd(a,b))
