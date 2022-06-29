c16582f88357f875070f1985c2161df0677ed474
deepakdeepu000 <99535087+deepakdeepu000@users.noreply.github.com>
2022-06-30 02:12:27 +0530

    n=int(input()) a=list(map(int,input().split())) c=0 l=0 s='' k=''
    for i in a:
        for j in a:
            if i==j:
                c+=1
        if c==i:
            l+=1
            s+=str(i)
        c=0
    if l==0:
        print("-1")
    else:
        for i in s:
            if i not in k:
                k+=i
        for j in k:
            print(j,end=" ")

--- Elements_same_as_count.py
+++ Elements_same_as_count.py
@@ -1,23 +1,25 @@
-a=int(input())
-b=[a]
-b=list(map(int,input().split()))
+n=int(input())
+a=list(map(int,input().split()))
 c=0
 l=0
-p=''
 s=''
-for i in range(0,a):
-    for j in range(0,a):
-        if b[i]==b[j]:
+k=''
+
+for i in a:
+    for j in a:
+        if i==j:
             c+=1
-    if c==b[i]:
+    if c==i:
         l+=1
-        s+=str(b[i])
+        s+=str(i)
     c=0
 if l==0:
     print("-1")
 else:
     for i in s:
-        if i not in p:
-            p=p+i
-    for j in p:
-        print(j,end=' ')
+        if i not in k:
+            k+=i
+    for j in k:
+        print(j,end=" ")
+
+    
