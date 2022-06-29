e468e0d3cb51a5ae3fa37a7f8116859699218d47
deepakdeepu000 <99535087+deepakdeepu000@users.noreply.github.com>
2022-06-29 22:37:46 +0530

    def rev(n):     c=0     while n!=0:         a=n%10         n=n//10         c+=a     return c n=int(input()) a=rev(n) if a>=10:     b=rev(a)     if b>=10:         c=rev(b)         print(c)     else:         print(b) else:     print(a)

--- Add_Digits.py
+++ Add_Digits.py
@@ -1,23 +1,18 @@
+def rev(n):
+    c=0
+    while n!=0:
+        a=n%10
+        n=n//10
+        c+=a
+    return c
 n=int(input())
-b=0
-d=0
-f=0
-while n!=0:
-    a=n%10
-    n=n//10
-    b+=a
-if b>=10:
-    while b!=0:
-        c=b%10
-        b=b//10
-        d+=c
-    if d>=10:
-        while d!=0:
-            e=d%10
-            d=d//10
-            f+=e
-        print(f)
+a=rev(n)
+if a>=10:
+    b=rev(a)
+    if b>=10:
+        c=rev(b)
+        print(c)
     else:
-        print(d)
+        print(b)
 else:
-    print(b)
+    print(a)
