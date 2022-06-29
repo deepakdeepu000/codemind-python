16d00b6b9606b0c1c23cbc8d1ac771b2ef07e2e8
deepakdeepu000 <99535087+deepakdeepu000@users.noreply.github.com>
2022-06-30 01:07:38 +0530

    a=int(input()) b=[a] b=list(map(int,input().split())) c=0 l=0 for i in range(0,a):     for j in range(i,a):         if b[i]==b[j]:             c+=1     if c==1:         if b[i]%2==0:             l+=b[i]     c=0 print(l)

---
+++ unique_even_elements_sum.py
@@ -0,0 +1,14 @@
+a=int(input())
+b=[a]
+b=list(map(int,input().split()))
+c=0
+l=0
+for i in range(0,a):
+    for j in range(i,a):
+        if b[i]==b[j]:
+            c+=1
+    if c==1:
+        if b[i]%2==0:
+            l+=b[i]
+    c=0
+print(l)
