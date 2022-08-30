1ee22ca2f93e5fb4951bcb22a5b3b52b09c67b21
deepakdeepu000 <99535087+deepakdeepu000@users.noreply.github.com>
2022-08-30 15:39:28 +0530

    n=input() s=n.lower() e=[] k='' d="abcdefghijklmnopqrstuvwxyz" for i in s:     if s.count(i)==1:         if i in d:             e.append(i) p=set(e) j=list(p) j.sort() for i in j:     k+=i print(len(k))

---
+++ count_unique_characters.py
@@ -0,0 +1,15 @@
+n=input()
+s=n.lower()
+e=[]
+k=''
+d="abcdefghijklmnopqrstuvwxyz"
+for i in s:
+    if s.count(i)==1:
+        if i in d:
+            e.append(i)
+p=set(e)
+j=list(p)
+j.sort()
+for i in j:
+    k+=i
+print(len(k))
