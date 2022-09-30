b74617a1847d784f608b2440db6c674dd727797f
deepakdeepu000 <99535087+deepakdeepu000@users.noreply.github.com>
2022-09-30 22:10:59 +0530

    a=input().lower() b=input().lower() s=[] for i in a:     if i==' ':         continue     if i not in b and i not in s:         s.append(i) for i in b:     if i==' ':         continue     if i not in a and i not in s:         s.append(i) print(len(s))

---
+++ Non_common_characters.py
@@ -0,0 +1,16 @@
+
+a=input().lower()
+b=input().lower()
+s=[]
+for i in a:
+    if i==' ':
+        continue
+    if i not in b and i not in s:
+        s.append(i)
+for i in b:
+    if i==' ':
+        continue
+    if i not in a and i not in s:
+        s.append(i)
+print(len(s))
+        
